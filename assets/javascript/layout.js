e25fa989158ec604b1d136d9a12e568c0952db70
nanette julius <nanettejulius@nanettes-MBP-3.attlocal.net>
2017-08-03 21:22:45 -0700

    updated HTML

--- assets/javascript/layout.js
+++ assets/javascript/layout.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
       $('.parallax').parallax();
     });
+var options = [
+       {selector: '#eventResults', offset: 400, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+    ];
+    Materialize.scrollFire(options);
+    
