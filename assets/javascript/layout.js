7a902888110a1a66c41d80fefc30d6b37cda35e1
nanette julius <nanettejulius@nanettes-MacBook-Pro-3.local>
2017-08-04 12:54:35 -0700

    updated page layout

--- assets/javascript/layout.js
+++ assets/javascript/layout.js
@@ -1,10 +1,14 @@
-$(document).ready(function(){
-      $('.parallax').parallax();
-    });
-var options = [
-       {selector: '#eventResults', offset: 400, callback: function(el) {
-        Materialize.showStaggeredList($(el));
-      } },
-    ];
-    Materialize.scrollFire(options);
-    
+$(document).ready(function() {
+  $('.parallax').parallax();
+
+
+  $('.collapsible').collapsible();
+});
+var options = [{
+  selector: '#eventResults',
+  offset: 400,
+  callback: function(el) {
+    Materialize.showStaggeredList($(el));
+  }
+}, ];
+Materialize.scrollFire(options);
