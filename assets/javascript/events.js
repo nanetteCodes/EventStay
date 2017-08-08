ee9ea4a2c94ab17cef7ce9fa53af2051f29ec303
Michael Pacheco <michaelpacheco@Michaels-MacBook-Pro.local>
2017-08-08 14:20:24 -0700

    working website

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -9,17 +9,21 @@
 var artist;
 var clientId = "&client_id=ODM4NTkyM3wxNTAxNzgxMjczLjM5";
 $("#eventResults").hide();
+var lat;
+var lon;
+var newList;
+
 
 // *Calling ajax for events
 // ==========================
 function findEvent(events){
 
 	// var for query url
-	var queryURL2 = "https://api.seatgeek.com/2/events?performers.slug=" + artist + clientId;
+	var queryURL = "https://api.seatgeek.com/2/events?performers.slug=" + artist + clientId;
 
 
 	$.ajax({
-      url: queryURL2,
+      url: queryURL,
       method: "GET"
     }).done(function(response2) {
 
@@ -29,7 +33,7 @@ function findEvent(events){
 // *Construct HTML 
 // ====================
 
-
+	// if prev searches... remove items 
 	$(".result").remove();
 
 
@@ -37,8 +41,8 @@ function findEvent(events){
 		var object = response2.events[i];
 
 	// set variables for info i need
-	var lat = object.venue.location.lat;
-	var lon = object.venue.location.lon;
+		lat = object.venue.location.lat;
+		lon = object.venue.location.lon;
 	var eventName = object.title;
 	var tickets = $("<a>").attr("href",object.url).text("Buy Tickets");
 		tickets.attr("target", "_blank");
@@ -47,11 +51,11 @@ function findEvent(events){
 
 
 
-	// Generating new div
-		var newList = $("<li>");
+	// Generating new div 
+			newList = $("<li>");
 		var newEvent = $("<div>");
 		$(newEvent).addClass("collapsible-header result");
-		$(newEvent).attr("id", "new-event");
+		$(newEvent).attr("data-Index", i);
 		$(newEvent).attr("lat", lat);
 		$(newEvent).attr("lon", lon);
 
@@ -71,12 +75,20 @@ function findEvent(events){
 	col2.append(tickets);
 
 
-	// append columns
+	// append columns to div
    	newEvent.append(col1, col2);
-   	newList.append(newEvent);
+
+    var body = $("<div>");
+	$(body).addClass("collapsible-body clearfix");
+	$(body).attr("id", "body"+i)
+   	// append div to new list
+   	newList.append(newEvent,body);
    	
 
    	$("#eventResults").append(newList);
+
+   // calling hoetel function
+   // hoeTels();
 
 };     
 });
@@ -139,7 +151,119 @@ $("#search").submit(function(event){
     // Running the findEvent function (passing in the artist as an argument)
     findEvent(event);
     findArtist(artist);
-
+    
 
 });
+
+
+$(document).on("click", ".result", function(){
+
+	 var body = $("<div>");
+	$(body).addClass("collapsible-body clearfix");
+
+	// $(this).append(body);
+
+	var lon= $(this).attr("lon");
+
+	var lat= $(this).attr("lat");
+
+	var id= $(this).attr("data-Index");
+
+var hotelRooms = ["assets/images/room1.jpg","assets/images/room2.jpg","assets/images/room3.jpg","assets/images/room4.jpg","assets/images/room5.jpg"]
+
+var queryURL = "https://hotwire.herokuapp.com/v1/deal/hotel?format=json&apikey=8bya58qw23u2q33c7cmwb34d&limit=10&dest=="+lat+","+lon+"&distance=*~30&starrating=4~*&sort=price&sortorder=asc";
+
+// var queryURL = "http://api.hotwire.com/v1/deal/hotel?apikey=8bya58qw23u2q33c7cmwb34d&format=json&limit=5&dest=="+lat+","+lon+"&distance=*~30&starrating=4~*&sort=price&sortorder=asc";
+
+
+
+
+$.ajax({
+		url:queryURL,
+		method: "GET"
+	})
+	.done(function(response){
+   
+
+
+
+		for (var i = 0; i < 5; i++) {
+			// console.log(response);
+
+		 var object2 = JSON.parse(response);
+			console.log(object2);
+			var price = object2.Result[i].Price;
+			var hotelURL = object2.Result[i].Url;
+			var starRating = object2.Result[i].StarRating;
+			var savingsPercentage = object2.Result[i].SavingsPercentage;
+			var city = object2.Result[i].City;
+			var state = object2.Result[i].StateCode;
+			var Neighborhood = object2.Result[i].Neighborhood;
+			var nights = object2.Result[i].NightDuration;
+
+			console.log(price, city, state);
+		
+
+		var hotelImg = $("<span>");
+		$(hotelImg).addClass("col s4");
+		$(hotelImg).attr("id","hotelImg");
+
+		var hotelPic = $("<img>");
+		hotelPic.attr("src", hotelRooms[i]);
+		hotelPic.attr("id","hotelPic");
+		hotelImg.append(hotelPic)
+
+
+		var hotelArea = $("<span>");
+		$(hotelArea).addClass("col s6");
+		$(hotelArea).attr("id","hotelinfo")
+		hotelArea.append("<b>"+Neighborhood+"</b><br/>");
+		hotelArea.append(city + ", "+ state);
+		hotelArea.append("<br/>Rating: " + starRating + " &#9734;&#9734;&#9734;&#9734;")
+		hotelArea.append("<br/>"+ "<a href='" + hotelURL + "' " + "> Hotwire LINK</a>" );
+
+		var priceInfo = $("<span>");
+		$(priceInfo).addClass("col s2");
+		$(priceInfo).attr("id","hotelPrices");
+		priceInfo.append("<h2>$" + Math.trunc(price) + "</h2><br/>Per Night</h1><br/>A " + savingsPercentage + "% Discount");
+			// console.log(priceInfo, hotelArea);
+		
+		var row = $("<div>");
+		$(row).addClass("row valign-wrapper");
+		row.append(hotelImg, hotelArea, priceInfo);
+
+		console.log(id);
+
+		$("#body"+id).append(row);
+
+			// console.log(row);
+
+		// body.append(row);
+		// 	// console.log(body);
+
+
+
+		// newList.append(body);
+			// console.log(newList);
+
+		// $("#eventResults").append(newList);
+
+};
+
+		
+
+});
+});
+
+
+
+
+
+// $(document).on('click','.result',function(){});
+
+
+
+
+
+
 
